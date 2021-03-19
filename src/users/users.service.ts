33235d7e7caa5a3229168dab1a8cb8ba01952fc8
thanhnguyenduy2610 <thanhnddgch17409@fpt.edu.vn>
2021-03-19 17:18:58 +0700

    update check unique name

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable, NotAcceptableException, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
@@ -23,9 +23,30 @@ export class UsersService {
     @InjectRepository(User)
     private readonly userRespository: Repository<User>,
   ){}
-   create(createUserDto: CreateUserDto) {
+    
+  
+  async create(createUserDto: CreateUserDto) {
     const user = this.userRespository.create(createUserDto);
-    return this.userRespository.save(user);
+
+    var userf = null;
+    const user1 = await this.userRespository.find();
+    user1.forEach(element => {
+        if ( user.Uname != element.Uname){
+           userf = user;
+        } 
+        else{
+          userf = null;
+        }
+    });
+    if(userf){
+      return this.userRespository.save(userf);
+    }
+    else
+    {
+      throw new NotAcceptableException(`already have user: ${user.Uname}`);
+      // return `already have user: ${user.Uname}`;
+    }
+    
   }
 
   findAll() {
